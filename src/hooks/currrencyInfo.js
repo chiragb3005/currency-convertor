3ca66615ab5cc6cd312794a7d8033925ab3fbc1b
Chirag Bansal <chiragb3005@gmail.com>
2026-04-08 11:51:15 +0530

    minute changes

--- src/hooks/currrencyInfo.js
+++ src/hooks/currrencyInfo.js
@@ -4,19 +4,22 @@ import {useState, useEffect, useId} from "react";
 function usecurrencyInfo(currency){
     const [data, setData] = useState({})
     // here an empty object inside the useState so i can put my data in the array
+    // later setData will update the value of data so we can return data at end
 
     // using of hook useEffect for fetching the data to keep it in cache
     // here is useEffect we give the dependency which can change or if its change we have to make change in UI or refresh of the cahche
     useEffect( () => {
         // using async so we can wait without any crash or fail
         const fetchData = async () => {
+            // using of try/catch makes the project more optimise
             try {
-                const res = fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
+                const res = await fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
                 const result = await res.json()
                 setData(result[currency]) 
             }
              catch (error) {
                 alert(`ERROR: problem faced during fetching of currecny ${currency}`)
+                // setting setData as an empty object so it doesnt crash
                 setData({});
             }
         }
@@ -24,6 +27,7 @@ function usecurrencyInfo(currency){
 
     } , [currency])
     return data;
+    // make sure to return the data for which we created the hook
 
 }
 
